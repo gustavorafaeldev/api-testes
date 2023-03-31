d2d7dfcd04308672b1ffe29bcdd46de353c8dd1b
gustavorafaeldev <grlrafael@outlook.com>
2023-03-31 10:11:08 -0300

    classe user resource findById

--- src/main/java/com/gustavo/api/resource/UserResource.java
+++ src/main/java/com/gustavo/api/resource/UserResource.java
@@ -1,6 +1,8 @@
 package com.gustavo.api.resource;
 
 import com.gustavo.api.domain.User;
+import com.gustavo.api.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
+@RequiredArgsConstructor
 public class UserResource {
+
+    private final UserService service;
 
     @GetMapping("{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(new User(1, "Rafael", "grlrafael@outlook.com", "123"));
+        return ResponseEntity.ok(service.findById(id));
     }
 }
