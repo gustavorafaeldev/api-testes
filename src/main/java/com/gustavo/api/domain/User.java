0e0f093f9dbaebdedd229b67b415005e5a878e41
gustavorafaeldev <grlrafael@outlook.com>
2023-03-31 09:50:38 -0300

    anotações para a classe

--- src/main/java/com/gustavo/api/domain/User.java
+++ src/main/java/com/gustavo/api/domain/User.java
@@ -1,12 +1,16 @@
 package com.gustavo.api.domain;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
