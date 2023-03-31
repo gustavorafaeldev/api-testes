cfdd07207f9055fc54413136eba394451195e961
gustavorafaeldev <grlrafael@outlook.com>
2023-03-31 09:44:12 -0300

    classe user

---
+++ src/main/java/com/gustavo/api/domain/User.java
@@ -0,0 +1,20 @@
+package com.gustavo.api.domain;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
