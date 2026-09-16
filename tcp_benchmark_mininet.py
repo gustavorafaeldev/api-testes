9676f402bf3deb12467eadec6a243e62bd68c2dc
Gustavo Rafael <105467235+gustavorafaeldev@users.noreply.github.com>
2026-09-15 23:44:40 -0300

    test: match single,2 Mininet topology

--- tcp_benchmark_mininet.py
+++ tcp_benchmark_mininet.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 import json
-import subprocess
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.log import setLogLevel
+from mininet.node import OVSBridge
+from mininet.topo import Topo
 
 setLogLevel('warning')
+
+class LabTopo(Topo):
+    def build(self, loss=0):
+        s1 = self.addSwitch('s1')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        opts = dict(bw=20, delay='20ms', loss=loss)
+        self.addLink(h1, s1, **opts)
+        self.addLink(h2, s1, **opts)
 
 def available_algorithms():
     return open('/proc/sys/net/ipv4/tcp_available_congestion_control').read().split()
 
 def run_scenario(label, loss):
-    net = Mininet(controller=None, link=TCLink, build=False)
-    h1 = net.addHost('h1')
-    h2 = net.addHost('h2')
-    net.addLink(h1, h2, bw=20, delay='20ms', loss=loss)
-    net.build()
+    topo = LabTopo(loss=loss)
+    net = Mininet(topo=topo, controller=None, link=TCLink, switch=OVSBridge)
     net.start()
+    h1, h2 = net.get('h1', 'h2')
     try:
         h1.cmd('iperf3 -s -D')
         algos = available_algorithms()
